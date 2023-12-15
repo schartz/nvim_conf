b92e42ea85f526c9386573d4b9c3a5c7d36843aa
Schartz Rehan <rehan.qt@gmail.com>
2023-12-15 12:58:34 +0530

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.g.python_host_prog = '/path/to/python'
+vim.g.python_host_prog = "/path/to/python"
+vim.opt.relativenumber = false
+
